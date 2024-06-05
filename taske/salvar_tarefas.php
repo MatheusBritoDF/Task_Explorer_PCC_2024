1b72252ffe97f3012763a30dd69089fd22ddaba1
Erick <165854538+Erick-3698@users.noreply.github.com>
2024-06-05 11:53:22 -0300

    Update and rename tarefas-save.php to salvar_tarefas.php

---
+++ taske/salvar_tarefas.php
@@ -0,0 +1,48 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Configurações do banco de dados
+$host = 'localhost';
+$dbname = 'db_task_explorer';
+$username = 'root';
+$password = '';
+
+// Tente estabelecer a conexão com o banco de dados usando PDO
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Configurar o PDO para lançar exceções em caso de erros
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Se ocorrer um erro ao conectar, exibir uma mensagem de erro e interromper o script
+    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
+}
+
+// Verificar se os dados foram enviados corretamente
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Obter os dados do formulário
+    $titulo = $_POST["titulo"];
+
+    try {
+        // Preparar e executar a consulta SQL para inserir a tarefa no banco de dados
+        $stmt = $pdo->prepare("INSERT INTO cartoes (titulo, descricao, nivel_prioridade, id_lista) VALUES (:titulo, NULL, NULL, 1)");
+        $stmt->bindParam(':titulo', $titulo);
+
+        if ($stmt->execute()) {
+            // Se a inserção for bem-sucedida, retornar uma mensagem de sucesso
+            echo "Tarefa salva com sucesso!";
+        } else {
+            // Se ocorrer um erro, retornar uma mensagem de erro
+            echo "Erro ao salvar a tarefa.";
+        }
+    } catch (PDOException $e) {
+        // Se ocorrer um erro ao executar a consulta, exibir uma mensagem de erro
+        echo "Erro ao executar a consulta: " . $e->getMessage();
+    }
+}
+
+// Fechar a conexão PDO
+$pdo = null;
+?>
